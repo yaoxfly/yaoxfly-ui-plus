c21ed8103ba57b44817a01a4e842164a86fccc6f
yaoxfly <1315892088@qq.com>
2025-01-14 16:28:01 +0800

    feat: 完善form表单组件，新增checkbox、datePicker等表单

--- packages/component-pc/src/component/form/src/form.tsx
+++ packages/component-pc/src/component/form/src/form.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, useAttrs, PropType } from 'vue'
+import { defineComponent, PropType } from 'vue'
 import { FormProps, FormItemPropsEl } from './props'
 import { useExpose } from '../../../hook'
 const COMPONENT_NAME = 'YxForm'
@@ -10,8 +10,8 @@ export default defineComponent({
       required: true
     }
   },
-  setup(props) {
-    const $attrs = useAttrs() as unknown as FormProps
+  setup(props, { attrs }) {
+    const $attrs = attrs as unknown as FormProps
     const refs = ref<{ [key: string]: any }>({})
     console.log($attrs, 'ddd')
     const getFormItem = ref<{ [key: string]: (item: FormItemPropsEl) => any }>({
@@ -34,14 +34,35 @@ export default defineComponent({
             ))}
           </el-radio-group>
         )
-      }
+      },
+      checkbox: (cItem) => {
+        const { checkboxGroup, vModel, ...other } = cItem.checkbox ?? {}
+        const { checkbox, checkboxButton, ...restCheckboxGroupProps } = checkboxGroup ?? {}
+        return (
+          <el-checkbox-group v-model={$attrs.model[cItem.checkbox?.vModel ?? '']} {...restCheckboxGroupProps} {...other} ref={(el: any) => setRef(el, cItem)}>
+            {checkbox && checkbox.map((item) => (
+              <el-checkbox key={item.value} {...item}>{item.label}</el-checkbox>
+            ))}
+
+            {checkboxButton && checkboxButton.map((item) => (
+              <el-checkbox-button key={item.value} {...item}>{item.label}</el-checkbox-button>
+            ))}
+          </el-checkbox-group>
+        )
+      },
+      datePicker: (cItem) => <el-date-picker v-model={$attrs.model[cItem.datePicker?.vModel ?? '']} {...cItem.datePicker} value-format={(cItem.datePicker && cItem.datePicker.valueFormat) ?? 'YYYY-MM-DD'} ref={(el: any) => setRef(el, cItem)} />,
+      timePicker: (cItem) => <el-time-picker v-model={$attrs.model[cItem.timePicker?.vModel ?? '']} {...cItem.timePicker} value-format={(cItem.timePicker && cItem.timePicker.valueFormat) ?? 'HH:mm:ss'} ref={(el: any) => setRef(el, cItem)} />,
+      timeSelect: (cItem) => <el-time-select v-model={$attrs.model[cItem.timeSelect?.vModel ?? '']} {...cItem.timeSelect} ref={(el: any) => setRef(el, cItem)} />,
+      cascader: (cItem) => <el-cascader v-model={$attrs.model[cItem.cascader?.vModel ?? '']} {...cItem.cascader} ref={(el: any) => setRef(el, cItem)} />,
+      autocomplete: (cItem) => <el-autocomplete v-model={$attrs.model[cItem.autocomplete?.vModel ?? '']} {...cItem.autocomplete} ref={(el: any) => setRef(el, cItem)} />,
+      treeSelect: (cItem) => <el-tree-select v-model={$attrs.model[cItem.treeSelect?.vModel ?? '']} {...cItem.treeSelect} ref={(el: any) => setRef(el, cItem)} />
     })
-
     const setRef = (el: any, cItem: FormItemPropsEl) => {
-      refs.value[cItem.ref] = el
+      if (cItem.ref) {
+        refs.value[cItem.ref] = el
+      }
     }
-
-    useExpose({ refs })
+    useExpose({ refs: refs.value })
     return () => (
       <div>
         <el-form {...$attrs} >
--- packages/component-pc/src/component/form/src/props.ts
+++ packages/component-pc/src/component/form/src/props.ts
@@ -1,28 +1,59 @@
 import type {
   FormItemProps as ElFormItemProps,
-  InputProps as ElInputProps,
   FormProps as ElFormProps,
-  ISelectProps,
   FormItemRule as ElFormItemRule,
   FormRules as ElFormRules,
+  // input
+  InputProps as ElInputProps,
+  InputInstance as ElInputInstance,
   InputNumberInstance as ElInputNumberInstance,
   InputEmits,
   InputNumberEmits,
+  // select
+  ISelectProps,
+  // radio
   RadioProps as ElRadioProps,
   RadioGroupProps as ElRadioGroupProps,
   RadioEmits,
-  RadioButtonProps
+  RadioGroupInstance as ElRadioGroupInstance,
+  RadioInstance as ElRadioInstance,
+  RadioButtonProps,
+  RadioButtonInstance as ElRadioButtonInstance,
+  // checkbox
+  CheckboxProps as ElCheckboxProps,
+  CheckboxGroupProps as ElCheckboxGroupProps,
+  CheckboxEmits,
+  CheckboxGroupInstance as ElCheckboxGroupInstance,
+  CheckboxInstance as ElCheckboxInstance,
+  // DatePicker
+  DatePickerProps as ElDatePickerProps,
+  DatePickerInstance as ElDatePickerInstance,
+  // TimePicker
+  TimePickerDefaultProps,
+  // TimeSelect
+  TimeSelectProps as ElTimeSelectProps,
+  TimeSelectInstance as ElTimeSelectInstance,
+  // Cascader
+  CascaderEmits as ElCascaderEmits,
+  CascaderInstance as ElCascaderInstance,
+  // Autocomplete
+  AutocompleteInstance as ElAutocompleteInstance,
+  AutocompleteEmits,
+  // Tree
+  TreeInstance as ElTreeInstance
 } from 'element-plus'
-
+import type Form from './form'
+export type CamelCase<S extends string> =
+  S extends `${infer T}-${infer U}${infer Rest}`
+  ? `${Capitalize<T>}${CamelCase<`${Capitalize<U>}${Rest}`>}`
+  : Capitalize<S>;
 
 export type InputProps = Partial<ElInputProps> & {
   vModel: string;
-  modifier:string[]
-
+  modifier: string[]
 } & {
-  [key in keyof InputEmits as `on${Capitalize<string & key>}`]: (...args: Parameters<InputEmits[key]>) => void;
+  [key in keyof InputEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<InputEmits[key]>) => void;
 };
-
 
 export type SelectEmit = {
   change: (value: any) => void;
@@ -33,48 +64,134 @@ export type SelectEmit = {
   'focus': (event: FocusEvent) => void;
 };
 
-
 export type SelectProps = Partial<ISelectProps> & {
   vModel: string;
-  options:{
+  options: {
     label?: string;
     value?: string;
     disabled?: boolean;
   }[];
-} & SelectEmit;
-
-
-export type InputNumberInstance = Partial<ElInputNumberInstance> & {
-  vModel: string;
 } & {
-  [key in keyof InputNumberEmits as `on${Capitalize<string & key>}`]: (...args: Parameters<InputEmits[key]>) => void;
+  [key in keyof SelectEmit as `on${CamelCase<string & key>}`]: (...args: Parameters<SelectEmit[key]>) => void;
 };
 
-
+export type InputNumberProps = Partial<ElInputNumberInstance['$props']> & {
+  vModel: string;
+} & {
+  [key in keyof InputNumberEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<InputEmits[key]>) => void;
+};
 
 export type radioProps = {
   vModel: string;
-  radioGroup:Partial<ElRadioGroupProps> & {
+  radioGroup: Partial<ElRadioGroupProps> & {
     radio?: Partial<ElRadioProps>[];
     radioButton?: Partial<RadioButtonProps>[];
-  } ;
-}& {
-  [key in keyof RadioEmits as `on${Capitalize<string & key>}`]: (...args: Parameters<RadioEmits[key]>) => void;
+  };
+} & {
+  [key in keyof RadioEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<RadioEmits[key]>) => void;
 };
 
 
+export type CheckboxProps = {
+  vModel: string;
+  checkboxGroup: Partial<ElCheckboxGroupProps> & {
+    checkbox?: Partial<ElCheckboxProps>[];
+    checkboxButton?: Partial<ElCheckboxProps>[];
+  };
+} & {
+  [key in keyof CheckboxEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<CheckboxEmits[key]>) => void;
+};
 
+export type DatePickerEmit = {
+  change: (value: any) => void;
+  'blur': (e: FocusEvent) => void
+  'focus': (e: FocusEvent) => void;
+  'clear': () => void;
+  'calendar-change': (val: [Date, null | Date]) => void
+  'panel-change': (date: Date | [Date, Date], mode: 'month' | 'year', view?: string) => void;
+  'visible-change': (visibility: boolean) => void;
+}
+
+
+export type DatePickerProps = Partial<ElDatePickerProps> & {
+  vModel: string;
+} & {
+  [key in keyof DatePickerEmit as `on${CamelCase<string & key>}`]: (...args: Parameters<DatePickerEmit[key]>) => void;
+};
+
+
+export type TimePickerEmit = {
+  change: (val: number | string | Date | [number, number] | [string, string] | [Date, Date]) => void;
+  'blur': (e: FocusEvent) => void
+  'focus': (e: FocusEvent) => void;
+  'clear': () => void;
+  'visible-change': (visibility: boolean) => void;
+}
+
+export type TimePickerProps = Partial<TimePickerDefaultProps> & {
+  vModel: string;
+} & {
+  [key in keyof TimePickerEmit as `on${CamelCase<string & key>}`]: (...args: Parameters<TimePickerEmit[key]>) => void;
+};
+
+
+export type TimeSelectEmit = {
+  change: (val: number | string | Date | [number, number] | [string, string] | [Date, Date]) => void;
+  'blur': (e: FocusEvent) => void
+  'focus': (e: FocusEvent) => void;
+  'clear': () => void;
+  // 'visible-change': (visibility: boolean) => void;
+}
+
+export type TimeSelectProps = Partial<ElTimeSelectProps> & {
+  vModel: string;
+} & {
+  [key in keyof TimeSelectEmit as `on${CamelCase<string & key>}`]: (...args: Parameters<TimeSelectEmit[key]>) => void;
+};
+
+export type CascaderProps = Partial<ElCascaderInstance['$props']> & {
+  vModel: string;
+} & {
+  [key in keyof ElCascaderEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<ElCascaderEmits[key]>) => void;
+};
+
+
+export type AutocompleteProps = Partial<ElAutocompleteInstance['$props']> & {
+  vModel: string;
+  placeholder: string
+} & {
+  [key in keyof AutocompleteEmits as `on${CamelCase<string & key>}`]: (...args: Parameters<AutocompleteEmits[key]>) => void;
+};
+
+
+type TreeEmitType = ElTreeInstance['$emit'];
+export type TreeEmit = {
+  [K in Parameters<TreeEmitType>[0]]: (...args: Extract<Parameters<TreeEmitType>, [K, ...any]>[1]) => void;
+};
+
+export type TreeSelectEmit = SelectEmit & TreeEmit
+export type TreeSelectProps = Partial<ElTreeInstance['$props'] & ISelectProps> & {
+  vModel: string;
+} & {
+  [key in keyof TreeSelectEmit as `on${CamelCase<string & key>}`]: (...args: Parameters<TreeSelectEmit[key]>) => void;
+};
 
 export interface FormItemPropsEl extends Partial<ElFormItemProps> {
-  ref?:any;
+  ref?: any;
   span?: number;
   type?: string;
   input?: Partial<InputProps>,
   select?: Partial<SelectProps>,
-  inputNumber?: Partial<InputNumberInstance>
-  radio?:Partial<radioProps>
+  inputNumber?: Partial<InputNumberProps>
+  radio?: Partial<radioProps>
+  checkbox?: Partial<CheckboxProps>
+  datePicker?: Partial<DatePickerProps>
+  timePicker?: Partial<TimePickerProps>
+  timeSelect?: Partial<TimeSelectProps>
+  cascader?: Partial<CascaderProps>
+  autocomplete?: Partial<AutocompleteProps>
+  treeSelect?: Partial<TreeSelectProps>
 }
-
 
 export type FormItemsProps = FormItemPropsEl[][];
 export type FormItemRule = ElFormItemRule;
@@ -82,8 +199,25 @@ export type FormRules<T> = {
   [K in keyof T]?: ElFormRules[number];
 };
 
-
 export interface FormProps extends Partial<ElFormProps> {
   formItems: FormItemsProps;
   model: Record<string, any>;
 }
+
+export type InputInstance = ElInputInstance
+export type InputNumberInstance = ElInputNumberInstance
+export type RadioGroupInstance = ElRadioGroupInstance
+export type RadioInstance = ElRadioInstance
+export type RadioButtonInstance = ElRadioButtonInstance
+export type CheckboxGroupInstance = ElCheckboxGroupInstance
+export type CheckboxInstance = ElCheckboxInstance
+export type DatePickerInstance = ElDatePickerInstance
+export type TimeSelectInstance = ElTimeSelectInstance
+export type CascaderInstance = ElCascaderInstance
+export type AutocompleteInstance = ElAutocompleteInstance
+export type TreeInstance = ElTreeInstance
+
+export type FormInstance<K extends string, T extends Record<K, any>> = InstanceType<typeof Form> & {
+  refs: T;
+};
+
