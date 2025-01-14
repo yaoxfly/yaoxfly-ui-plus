519c99a7748d52e2d0ffdc2ab2291d91ced4077c
yaoxfly <1315892088@qq.com>
2025-01-14 16:22:51 +0800

    chore: 国际化配置

--- examples/demo/src/main.ts
+++ examples/demo/src/main.ts
@@ -4,14 +4,16 @@ import { createPinia } from 'pinia'
 import '@/assets/css/index.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 // import { Button } from '@yaoxfly/component-pc'
 // console.log(Test)
 import ComponentPc from '../../../packages/component-pc/index'
 import '../../../packages/theme-chalk/src/index.scss'
-
 createApp(App)
   .use(router)
   .use(createPinia())
-  .use(ElementPlus)
+  .use(ElementPlus, {
+    locale: zhCn
+  })
   .use(ComponentPc)
   .mount('#app')
